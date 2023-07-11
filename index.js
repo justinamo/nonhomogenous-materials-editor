bf3ed70211d73cb50652f0fbc0c0a34c8209fd2b
justin <justinamo@gmail.com>
2023-07-11 16:42:14 -0400

    read svg

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var gridCellSize = 150; 
-var inchesPerGridCell = 5;
+var inchesPerGridCell = 4;
 var originOffsetX = 0; 
 var originOffsetY = 0; 
 var canvasDragStartX = null; 
 var canvasDragStartY = null; 
 
 var plateData; 
-var plateWidthInInches = 12;
-var plateHeightInInches = 19; 
+var plateWidthInInches;
+var plateHeightInInches; 
+var readingPlate = false;
+
+var svgData; 
+var readingSvg = false;
 
 var view = document.getElementById("view");
 var viewHeight = 800; 
@@ -19,6 +23,22 @@ var viewsizerDragged = false;
 canvas = document.getElementById("canvas");
 canvasOffset = 40;
 var canvasDragged = false; 
+
+async function upload(formData, url) {
+  try {
+    const response = await fetch("http://localhost:8080/" + url, {
+      method: "PUT",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: formData,
+    });
+    const result = await response.json();
+    console.log("Success:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 function startup() { 
   view.addEventListener("mousedown", (event) => {
@@ -38,29 +58,25 @@ function startup() {
       viewHeight = event.y - 50;
       updateViewSize();
       updateCanvasSize();
-      drawGrid(gridCellSize, inchesPerGridCell);
-      drawPlate();
+      draw(); 
     };
     if (canvasDragged && !viewsizerDragged) {
       originOffsetX = event.x - canvasDragStartX;
       originOffsetY = event.y - canvasDragStartY;
       clearCanvas();
-      drawGrid(gridCellSize, inchesPerGridCell);
-      drawPlate();
+      draw(); 
     };
   });
 
   window.addEventListener("mouseup", (event) => {
     if (viewsizerDragged) { 
       updateCanvasSize();
-      drawGrid(gridCellSize, inchesPerGridCell);
-      drawPlate();
+      draw();
     };
     viewsizerDragged = false;
     if (canvasDragged) {
       clearCanvas();
-      drawGrid(gridCellSize, inchesPerGridCell);
-      drawPlate(); 
+      draw(); 
     };
     canvasDragged = false; 
   }); 
@@ -68,20 +84,46 @@ function startup() {
   updateCanvasSize(); 
   drawGrid(gridCellSize, inchesPerGridCell);
 
-  var reader = new FileReader(); 
-  reader.addEventListener("load", (event) => {
+  var plateReader = new FileReader(); 
+  plateReader.addEventListener("load", (event) => {
     var image = new Image(); 
-    image.src = reader.result; 
-    console.log("appending");
+    image.src = plateReader.result; 
     plateData = image; 
-    drawPlate(); 
+    plateWidthInInches = parseFloat(document.getElementById("width-input").value);
+    plateHeightInInches = parseFloat(document.getElementById("height-input").value);
+    clearCanvas();
+    draw(); 
+  });
+
+  var svgReader = new FileReader(); 
+  svgReader.addEventListener("load", (event) => {
+    var image = new Image(); 
+    image.src = svgReader.result; 
+    svgData = image; 
+    draw(); 
   });
 
   var photoInput = document.getElementById("photo-input"); 
   photoInput.addEventListener("change", (event) => {
     if (photoInput.files && photoInput.files[0]) { 
-      photo = photoInput.files[0]; 
-      reader.readAsDataURL(photo);
+      var photo = photoInput.files[0]; 
+      plateReader.readAsDataURL(photo);
+    };
+  });
+
+  var svgInput = document.getElementById("svg-input"); 
+  svgInput.addEventListener("change", (event) => {
+    if (svgInput.files && svgInput.files[0]) { 
+      var svg = svgInput.files[0]; 
+
+      const formData = new FormData(); 
+      formData.append("svg", svg); 
+      upload(formData, "svg").then(() => {
+	console.log("success");
+      }, () => {
+	console.log("failure");
+      });
+      //svgReader.readAsDataURL(svg);
     };
   });
 };
@@ -122,7 +164,7 @@ function drawGrid(cellSize, inchesPerCell) {
     for (let i = originOffsetX > 0 ? 0 : 1 ; i <= linesToDrawX; i++) {
       horizontalPosition = cellSize * i + canvasOffset + (originOffsetX % cellSize);
       ctx.textAlign = "center";
-    ctx.textBaseline = "alphabetic";
+      ctx.textBaseline = "alphabetic";
       var digit_i = originOffsetX >= 0 ? i : i - 1;
       ctx.fillText((digit_i - Math.floor(originOffsetX / cellSize)) * inchesPerCell, horizontalPosition, 30);
       ctx.moveTo(horizontalPosition, 0 + canvasOffset); 
@@ -144,21 +186,48 @@ function drawGrid(cellSize, inchesPerCell) {
   };
 };
 
+function drawImage(ctx, imageData) { 
+  dx = Math.max(originOffsetX + canvasOffset, canvasOffset); 
+  dy = Math.max(originOffsetY + canvasOffset, canvasOffset); 
+  // TODO: should handle other widths in addition to plate width
+  gridWidth = plateWidthInInches / inchesPerGridCell * gridCellSize; 
+  gridHeight = plateHeightInInches / inchesPerGridCell * gridCellSize; 
+  sx = Math.max(0, -originOffsetX) * imageData.width / gridWidth;
+  sy = Math.max(0, -originOffsetY) * imageData.height / gridHeight;
+  sWidth = Math.min(gridWidth, viewWidth + canvasOffset - dx) * imageData.width / gridWidth;
+  sHeight = Math.min(gridHeight, viewHeight + canvasOffset - dy) * imageData.height / gridHeight;
+  dWidth = Math.min(gridWidth, viewWidth + canvasOffset - dx);
+  dHeight = Math.min(gridHeight, viewHeight + canvasOffset - dy);
+  ctx.drawImage(imageData, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight); 
+};
+
 function drawPlate() { 
-  if (canvas.getContext) {
+  if (canvas.getContext && plateData) {
     const ctx = canvas.getContext("2d");
-    dx = Math.max(originOffsetX + canvasOffset, canvasOffset); 
-    dy = Math.max(originOffsetY + canvasOffset, canvasOffset); 
-    gridWidth = plateWidthInInches / inchesPerGridCell * gridCellSize; 
-    gridHeight = plateHeightInInches / inchesPerGridCell * gridCellSize; 
-    sx = Math.max(0, -originOffsetX) * plateData.width / gridWidth;
-    sy = Math.max(0, -originOffsetY) * plateData.height / gridHeight;
-    sWidth = Math.min(gridWidth, viewWidth + canvasOffset - dx) * plateData.width / gridWidth;
-    sHeight = Math.min(gridHeight, viewHeight + canvasOffset - dy) * plateData.height / gridHeight;
-    dWidth = Math.min(gridWidth, viewWidth + canvasOffset - dx);
-    dHeight = Math.min(gridHeight, viewHeight + canvasOffset - dy);
-    ctx.drawImage(plateData, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight); 
+    drawImage(ctx, plateData); 
+    rx = Math.min(dx, canvasOffset + viewWidth); 
+    ry = Math.min(dy, canvasOffset + viewHeight); 
+    rectWidth = Math.max(Math.min(dWidth, gridWidth + originOffsetX), 0);
+    rectHeight = Math.max(Math.min(dHeight, gridHeight + originOffsetY), 0);
+    ctx.beginPath();
+    ctx.rect(rx, ry, rectWidth, rectHeight);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.closePath();
   };
+};
+
+function drawSvg() { 
+  if (canvas.getContext && svgData) { 
+    const ctx = canvas.getContext("2d");
+    drawImage(ctx, svgData);
+  };
+};
+
+function draw() {
+  drawGrid(gridCellSize, inchesPerGridCell);
+  drawPlate();
+  drawSvg(); 
 };
 
 startup();
